7c48a67410771c1ac382f6236d11d939ea8a5f60
Akinsiku Femi <akinsiku13@gmail.com>
2020-05-22 15:57:18 +0100

    Add flash message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,16 +9,18 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article = Article.new
+         
     end
 
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
-        @article.save
+        if @article.save
+            flash[:notice] = "Article was created successfully."
         # redirect_to article_path(@article) or
-        redirect_to @article
-
-
+            redirect_to @article
+        else 
+           render 'new' 
+        end
     end 
-
-
 end
