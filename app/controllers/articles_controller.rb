3df3b9fba224327f93caa1068dabc0ca77c93baa
Akinsiku Femi <akinsiku13@gmail.com>
2020-05-22 22:36:45 +0100

    Add edit controller and action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,8 +9,11 @@ class ArticlesController < ApplicationController
     end
 
     def new
-        @article = Article.new
-         
+        @article = Article.new  
+    end
+
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
@@ -23,4 +26,14 @@ class ArticlesController < ApplicationController
            render 'new' 
         end
     end 
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else 
+            render 'edit'
+        end
+    end
 end
