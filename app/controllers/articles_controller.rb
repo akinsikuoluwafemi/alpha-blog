8de470e696fe8a0283c7a35f1592b6db0f5a4ae1
Akinsiku Femi <akinsiku13@gmail.com>
2020-05-22 09:06:03 +0100

    Implement the create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,9 +5,20 @@ class ArticlesController < ApplicationController
     end
 
     def index
-        @articles = Article.all
-        
+        @articles = Article.all  
     end
+
+    def new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        # redirect_to article_path(@article) or
+        redirect_to @article
+
+
+    end 
 
 
 end
