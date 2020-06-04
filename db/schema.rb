71e0fd58ad486b9aa32b1ba0bae2fe47a272f24d
Akinsiku Femi <akinsiku13@gmail.com>
2020-06-04 02:22:00 +0100

    Create password column in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_150833) do
+ActiveRecord::Schema.define(version: 2020_06_04_010042) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_06_03_150833) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
